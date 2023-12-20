5015ff7eca7ddded06a520e645a985caaffe460a
franznkemaka <franz@rigle.co>
2023-12-20 10:07:35 +0100

    🐳 k8s pod

--- code/hello-world/frontend/frontend.py
+++ code/hello-world/frontend/frontend.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     
-    backend_response =  requests.get("http://backend:80").text
+    # backend_response =  requests.get("http://backend:80").text
+    backend_response = "Backend is coming later!"
     return f'<html style="background:green;">Hello World, The backend said: {backend_response} </html>'
 
 app.run(host='0.0.0.0', port=80, debug=True)
